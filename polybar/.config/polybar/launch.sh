d87dff7bd0653aa9a9f4f1d3d8e65685efe7575b
dsul <dsul@users.noreply.github.com>
2023-05-08 22:13:26 -0400

    Update second monitor input

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,7 +9,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
 MONITOR=DP-4 polybar main -l info -c ~/.config/polybar/config.ini >> /tmp/polybar1.log 2>&1 &
-MONITOR=HDMI-0 polybar main -c ~/.config/polybar/config.ini >> /tmp/polybar1.log 2>&1 &
+MONITOR=DP-0 polybar main -c ~/.config/polybar/config.ini >> /tmp/polybar1.log 2>&1 &
 
 # launch bar
 #echo "---" | tee -a /tmp/polybar1.log
@@ -17,4 +17,3 @@ MONITOR=HDMI-0 polybar main -c ~/.config/polybar/config.ini >> /tmp/polybar1.log
 #MONITOR=HDMI-0 polybar bottom >>/tmp/polybar1.log 2>&1 &
 
 echo "Bars launched..."
-
